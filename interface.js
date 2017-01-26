c0e1d33e9cddacebbebe115337b40254c08e22ed
Parth <sparthcp@gmail.com>
2017-01-26 20:36:15 +0530

    Chapter 1 homework solved
    There was a n erro while testing the interface.js using test.js. The
    issue was with the fact that I was calling the callback after checking
    for error. Apperently, it was not suppose to be that way. I was suppose
    to call the callback directly and all the nullability of error is
    checked at the place where callback is used.
    
    Also there are files for chapter 2 added.

--- interface.js
+++ interface.js
@@ -1,14 +1,31 @@
 /*
+ * There is an error while testing this code:
+ * Timeout of 2000ms exceeded. Ensure that done() callback is being called in
+ * this test
+ * An initial research over this error suggests that the issue is with the
+ * memory leak in tests.
+ * refer following for further details
+ * https://github.com/mochajs/mocha/issues/2025
+ * 26/1/17 Evening
+ * The good news is that the error is solved
+ * the issue was an additional error checking before calling the callback. I
+ * think we are not suppose to check the nullablility of the arguments of the
+ * callbacks when defining them. We are already doing it while using them
+ *
+ * It's so surprising that a solution is always so simple...we keep searching for
+ * complex solutions considering various scenarios but a solution and hence
+ * an error....is
+ * always a simple one.
+ */
+
+/*
  *  Inserts "doc" into the collection "movies".
  */
 exports.insert = function(db, doc, callback) {
   // TODO: implement
   db.collection("movies").insert(doc, function(err, res){
-    if(err){
-       return callback(err);
-    }
+       callback(err);
   });
-
 };
 
 /*
@@ -19,5 +36,7 @@ exports.insert = function(db, doc, callback) {
  */
 exports.byDirector = function(db, director, callback) {
   // TODO: implement
-  callback(null, []);
+  db.collection("movies").find({'director':director}).sort({'title':1}).toArray(function(err, docs){
+      callback(err, docs);
+  });
 };
